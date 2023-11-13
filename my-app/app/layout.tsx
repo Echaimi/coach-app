76694d76cc44987d1a7a2f2f7b02aabd4eaa9642
unknown <echaimi@myges.fr>
2023-11-13 12:20:32 +0100

    core project

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "CoachHub",
+  description: "CoachHub Booking",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
